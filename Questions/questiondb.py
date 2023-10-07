80f89f7e74436926f3da3d6ec6ca671931b1ed39
Rijumas-Oscar <85748915+maumallma1@users.noreply.github.com>
2023-10-07 15:03:08 -0500

    Creacion_carpetas

---
+++ Questions/questiondb.py
@@ -0,0 +1,26 @@
+import random
+preguntas_respuestas = {
+    "¿Cuál es la capital de Francia?": {
+        "opciones": ["Londres", "París", "Berlín", "Madrid"],
+        "respuesta_correcta": "París"
+    },
+    "¿Cuál es el planeta más grande del sistema solar?": {
+        "opciones": ["Tierra", "Marte", "Júpiter", "Saturno"],
+        "respuesta_correcta": "Júpiter"
+    },
+    "¿Quién escribió 'Romeo y Julieta'?": {
+        "opciones": ["William Shakespeare", "Charles Dickens", "Jane Austen", "Homer"],
+        "respuesta_correcta": "William Shakespeare"
+    },
+    "¿Cuántos lados tiene un triángulo?": {
+        "opciones": ["3", "4", "5", "6"],
+        "respuesta_correcta": "3"
+    },
+    # Agrega más preguntas con opciones y respuesta común aquí
+}
+def obtener_pregunta_aleatoria():
+    pregunta, datos = random.choice(list(preguntas_respuestas.items()))
+    opciones = datos["opciones"]
+    respuesta_correcta = datos["respuesta_correcta"]
+    random.shuffle(opciones)  # Mezcla las opciones aleatoriamente
+    return pregunta, opciones, respuesta_correcta
